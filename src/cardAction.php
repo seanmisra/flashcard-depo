16fa70f0db904e605dae231a772244d8910c81f1
Sean Misra <seanmisra30@gmail.com>
2023-01-22 18:40:27 -0500

    Insert flashcard into cards table

--- src/cardAction.php
+++ src/cardAction.php
@@ -3,11 +3,36 @@
     $flashcardBack = "";
     $flashcardTags = "";
 
+    $user = 'root';
+    $pass = 'root';
+    $dbhost = 'mysql';
+    $database = 'mydatabase';
+
+    $conn = new PDO("mysql:host=$dbhost;database=$database", $user, $pass);     
+
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $flashcardFront = $_POST["flashcard-front"];
         $flashcardBack = $_POST["flashcard-back"];
         $flashcardTags = $_POST["flashcard-tags"];
+        $is_private = 'Y';
+
+
+
+        #echo "INSERT INTO mydatabase.cards (front_desc, back_desc, tags, is_private) VALUES ('test-front', 'test-back', 'test-tag', 'Y')";
+        #echo "<br>";
+        $sql = "INSERT INTO mydatabase.cards (front_desc, back_desc, tags, is_private) VALUES ('"
+            . $flashcardFront . "', '" . $flashcardBack . "', '" . $flashcardTags . "', '" . $is_private . "')";
+        #echo $sql;
+
+        try {
+            $conn->query($sql);
+        } catch (PDOException $e) {
+            echo "<br>";
+            die($e->getMessage());
+        }
+        echo "New record successfully created";
     }
+
 ?>
 
 <div>
