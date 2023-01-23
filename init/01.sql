947b13902b24fb1013408ee8fb10e8295602ebca
Sean Misra <seanmisra30@gmail.com>
2023-01-22 20:18:39 -0500

    Integrate SQL schemas with docker

---
+++ init/01.sql
@@ -0,0 +1,1 @@
+CREATE TABLE `mydatabase`.`cards` (`id` INT NOT NULL AUTO_INCREMENT , `front_desc` VARCHAR(1000) NOT NULL , `back_desc` VARCHAR(1000) NOT NULL , `tags` VARCHAR(500) NOT NULL , `is_private` CHAR(1) NOT NULL DEFAULT 'Y' , `user` VARCHAR(100) NULL DEFAULT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
